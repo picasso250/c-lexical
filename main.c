7b096b3968f292d138a9c3b6f6943e6d2beb5516
Ryan Wang <ryanwang@anjuke.com>
2014-05-12 11:31:23 +0800

    move to transfer table

--- main.c
+++ main.c
@@ -2,14 +2,19 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 #define MACHINE_INIT 0
-#define MACHINE_LAST_STATE -1
-#define MACHINE_ERROR -2
+#define MACHINE_ERROR -1
+#define MACHINE_LAST_STATE -2
 #define MACHINE_COMMENT_READY 1
 #define MACHINE_COMMENT_MULTI_LINE 11
 #define MACHINE_COMMENT_SINGLE_LINE 12
 #define MACHINE_COMMENT_MULTI_LINE_END_READY 12
+
+#define MACHINE_STATE_COUNT 8
+
+#define CHAR_ANY -1
 
 int read_file_name_from_arg(char * file_name)
 {
@@ -27,18 +32,47 @@ int transfer_table[][3] = {
 	{MACHINE_INIT, '/', MACHINE_COMMENT_READY},
 	{MACHINE_COMMENT_READY, '*', MACHINE_COMMENT_MULTI_LINE},
 	{MACHINE_COMMENT_READY, '/', MACHINE_COMMENT_SINGLE_LINE},
-	{MACHINE_COMMENT_READY, -1, MACHINE_ERROR},
+	{MACHINE_COMMENT_READY, CHAR_ANY, MACHINE_ERROR},
 	{MACHINE_COMMENT_MULTI_LINE, '*', MACHINE_COMMENT_MULTI_LINE_END_READY},
-	{MACHINE_COMMENT_MULTI_LINE_END_READY, -1, MACHINE_COMMENT_MULTI_LINE_END_READY},
+	{MACHINE_COMMENT_MULTI_LINE_END_READY, CHAR_ANY, MACHINE_COMMENT_MULTI_LINE_END_READY},
 	{MACHINE_COMMENT_MULTI_LINE_END_READY, '/', MACHINE_LAST_STATE},
 	{MACHINE_COMMENT_SINGLE_LINE, '\n', MACHINE_LAST_STATE},
 };
-int callback_map[] = {
 
+// array of pointers of function which take a char and returns an int
+int (*callback_map[])(char) = {
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
 };
 int machine_state;
 int machine_last_state;
 int line_num;
+int find_transfer_entry(int machine_state, int c)
+{
+	int i;
+	for (i = 0; i < MACHINE_STATE_COUNT; ++i)
+	{
+		int state = transfer_table[i][0];
+		int ch = transfer_table[i][1];
+		if (state == machine_state && (c == ch || ch == CHAR_ANY))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
 int state_machine_eat_char(char c)
 {
 	if (c == '\n')
@@ -47,70 +81,19 @@ int state_machine_eat_char(char c)
 	}
 	printf("machine_state %d\n", machine_state);
 	// printf("we read char of %c\n", c);
-	if (machine_state == MACHINE_INIT)
+	int i = find_transfer_entry(machine_state, c);
+	if (i == -1) // can not find
 	{
-		if (c == '/')
-		{
-			machine_state = MACHINE_COMMENT_READY;
-			printf("we are ready to comment \n");
-		}
+		printf("no state %d in transfer_table\n", machine_state);
+		return -1;
 	}
-	else if (machine_state == MACHINE_COMMENT_READY)
+	// call back
+	int (*func_cb)(char) = callback_map[i];
+	if (func_cb != NULL)
 	{
-		if (c == '*')
-		{
-			machine_state = MACHINE_COMMENT_MULTI_LINE;
-			printf("MACHINE_COMMENT_MULTI_LINE start\n");
-		}
-		else if (c == '/')
-		{
-			machine_state = MACHINE_COMMENT_SINGLE_LINE;
-			printf("MACHINE_COMMENT_SINGLE_LINE start\n");
-		}
-		else
-		{
-			printf("error, expect '/*' or '//' on line %d\n", line_num);
-			return 1;
-		}
-		return 0;
+		return func_cb(c);
 	}
-	else if (machine_state == MACHINE_COMMENT_MULTI_LINE)
-	{
-		if (c == '*')
-		{
-			machine_state = MACHINE_COMMENT_MULTI_LINE_END_READY;
-		}
-		else
-		{
-			putchar(c);
-		}
-	}
-	else if (machine_state == MACHINE_COMMENT_MULTI_LINE_END_READY)
-	{
-		if (c == '/')
-		{
-			machine_state = MACHINE_INIT; // todo old state
-		}
-		else if (c == '*')
-		{
-			machine_state = MACHINE_COMMENT_MULTI_LINE_END_READY;
-		}
-		else
-		{
-			machine_state = MACHINE_COMMENT_MULTI_LINE;
-		}
-	}
-	else if (machine_state == MACHINE_COMMENT_SINGLE_LINE)
-	{
-		if (c == '\n')
-		{
-			machine_state = MACHINE_INIT; // todo old state
-		}
-		else
-		{
-			putchar(c);
-		}
-	}
+	return 0;
 }
 int main(int argc, char const *argv[])
 {
